4ba1efab2dc026eacf262c34168e058f61dc0163
funkosaur <mario.bojkovski@outlook.com>
2022-03-19 15:40:12 +0000

    changing takingforminfo file

--- src/functions/takingFormInfo.js
+++ src/functions/takingFormInfo.js
@@ -1,23 +1,29 @@
-const submitButton = document.querySelector("#submitButton");
-const form = document.querySelector("#formContent")
-const projectTitle = document.querySelector("#projectTitle");
-const projectDescription = document.querySelector("#projectDescription");
-const projectDueDate = document.querySelector("#projectDueDate");
-const projectPriority = document.querySelector("#projectPriorityCheckbox1");
-const projectNotes = document.querySelector("#projectNotes")
+const projectFactory = (title, description, dueDate, priority, notes) => {
+  return { title, description, dueDate, priority, notes };
+};
+
 const projects = [];
 
-const projectFactory = (title, description, dueDate, priority, notes) => {
-    return {title, description, dueDate, priority, notes}
-}
-
 function processFormInfo(event) {
-    event.preventDefault();
-    const newProject = projectFactory(projectTitle.value, projectDescription.value, projectDueDate.value, projectPriority.value, projectNotes.value)
-    projects.push(newProject);
-    this.reset()
-    console.log(projects)
+  const projectTitle = document.querySelector("#projectTitle");
+  const projectDescription = document.querySelector("#projectDescription");
+  const projectDueDate = document.querySelector("#projectDueDate");
+  const projectPriority = document.querySelector("#projectPriorityCheckbox1");
+  const projectNotes = document.querySelector("#projectNotes");
+
+  event.preventDefault();
+
+  const newProject = projectFactory(
+    projectTitle.value,
+    projectDescription.value,
+    projectDueDate.value,
+    projectPriority.value,
+    projectNotes.value
+  );
+
+  projects.push(newProject);
+  this.reset();
+  console.log(projects);
 }
 
-form.addEventListener("submit", processFormInfo);
-
+export { processFormInfo, projects };
--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 import './styles/style.css';
 import formPage from "./pages/formPage.js";
+import { processFormInfo, projects} from "./functions/takingFormInfo.js";
+
 
 formPage()
-
-const submitButton = document.querySelector("#submitButton");
-
-const projectTitle = document.querySelector("#projectTitle");
-const projectDescription = document.querySelector("#projectDescription");
-const projectDueDate = document.querySelector("#projectDueDate");
-const projectPriority = document.querySelector("#projectPriorityCheckbox1");
-const projectNotes = document.querySelector("#projectNotes")
-const projects = [];
-
-const projectFactory = (title, description, dueDate, priority, notes) => {
-    return {title, description, dueDate, priority, notes}
-}
-
-projectTitle.addEventListener("click", ()=>{console.log("asdasfasd")})
-
-
-console.log(submitButton)
-form.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    console.log("sdfsf")
-    
-});
+const form = document.querySelector("#formContent");
+form.addEventListener("submit", processFormInfo);
